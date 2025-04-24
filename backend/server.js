7b0f0728b5db53b770a19914a341682c79a14909
SigmatoDev <nuthan.raghunath@sigmato.com>
2025-04-24 16:44:03 +0530

    env

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(cors({
-  origin: ['https://collegeseek.in'], // or '*' if public
+  origin: process.env.CLIENT_URL, // or '*' if public
   credentials: true
 }));
 app.use(express.json());
