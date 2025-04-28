18e1c8ad2cda277107b2cb99c983cdee11b1bd53
vishwas <vishwas.sigmato@gmail.com>
2025-04-28 18:42:51 +0530

    first commit

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['localhost'], // Allow images from localhost
-  },
-};
-
 // const nextConfig: NextConfig = {
 //   images: {
-//     domains: ['api.collegeseek.in'],
+//     domains: ['localhost'], // Allow images from localhost
 //   },
 // };
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['api.collegeseek.in'],
+  },
+};
 
 
 export default nextConfig;
