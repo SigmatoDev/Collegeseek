17292facc9e645ee26adbd2050210b5abe67a8f4
vishwas <vishwas.sigmato@gmail.com>
2026-04-15 02:37:14 +0530

    icon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Favicon from "@/components/favicon/Favicon";
 
-const geistSans = Geist({
+const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = JetBrains_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -20,16 +20,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black`}
       >
-       <Favicon /> {/* Include the Favicon component */}
-
+        <Favicon />
         {children}
       </body>
     </html>
